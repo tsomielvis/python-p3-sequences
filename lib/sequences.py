634d216e10a7d98f41448fb445ce1a5546e190f5
tsomielvis@gmail.com <tsomielvis@gmail.com>
2024-09-03 14:11:53 +0300

    completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+     if length == 0:
+        print([])
+     elif length == 1:
+        print([0])
+     elif length == 2:
+        print([0, 1])
+     else:
+        fib = [0,1]
+        for i in range(length-2):
+            fib.append(fib[len(fib)-1] + fib[len(fib)-2])
+        print(fib)
+    
